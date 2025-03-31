85402b6061b2bf079fa19569769c0ec8765b4699
Qaiem <qaiem575@gmail.com>
2025-03-31 09:30:12 +0500

    Update Directory and Codes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-3xl font-bold'>Developers Connect</h1>
+        <h1 className='text-2xl font-bold'>Developers Connect</h1>
       </div>
     </>
   )
