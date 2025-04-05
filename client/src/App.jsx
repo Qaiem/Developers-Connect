51b347abaa11755c81389d6e4d2aa278f2b0f8f8
Qaiem <qaiem575@gmail.com>
2025-04-06 03:52:26 +0500

    Working with Fronted Auth Client Side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import Footer from '../components/Footer'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Login from '../components/auth/login'
-import Register from '../components/auth/Register'
-
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Footer from '../components/Footer';
+import Login from '../components/auth/login';
+import Register from '../components/auth/Register';
 
 function App() {
+  
   return (
-     <Router>
-      <div>
-        <Navbar />
-        <Route exact path='/' Component={Hero} />
-        <div className='container'>
-          <Route path='/login' Component={Login} />
-          <Route path='/register' Component={Register} />
-        </div>
-        <Footer />
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
       </div>
-      </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ], 
   server: {
     proxy: {
       "/api": {
