2cca111dbff9031dac73f30c81790e1623cd4a72
Qaiem <qaiem575@gmail.com>
2025-04-10 11:00:11 +0500

    Updating State Mangement

--- client/components/auth/Register.jsx
+++ client/components/auth/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Register = () => {A
+const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
